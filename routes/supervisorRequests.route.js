4bea438fab914147ece8537554e4849a38d6ae1c
HKariyawasam <hasanigayani@gmail.com>
2022-05-21 23:35:36 +0530

    created delete Supervisor requests route function

--- routes/supervisorRequests.route.js
+++ routes/supervisorRequests.route.js
@@ -7,7 +7,7 @@ router.post('/', supervisorRequestsController.createSupervisorRequest);
 router.get('/', supervisorRequestsController.getAllRequests);
 router.get('/:name', supervisorRequestsController.getAllRequestsofSupervisor);
 router.put('/:id', supervisorRequestsController.updateSupervisorRequest);
-router.delete('/:id', supervisorRequestsController.updateSupervisorRequest);
+router.delete('/:id', supervisorRequestsController.deleteSupervisorRequest);
 
 
 module.exports = router;
