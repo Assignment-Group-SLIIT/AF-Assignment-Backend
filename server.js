7255c14c58eeb5508421cce1f862e720e256552c
Kaveen LK <kmklaksitha@gmail.com>
2022-05-21 17:12:36 +0530

    server file created and connected DB

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+
+const port = process.env.PORT || 4000;
+const URL = process.env.URL;
+
+app.use(express.urlencoded({ extended: false }))
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(URL, {
+    // useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useFindandModify: false
+});
+
+
+app.listen(port, () => {
+    console.log(`Server Is Running on Port: ${port}`);
+});
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+    console.log("Mongodb Connection success!");
+})
