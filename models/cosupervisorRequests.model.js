9676b995c929de7c90a2c48529706b31f5d628db
Thisara Thathsarani <thisararpg@gmail.com>
2022-05-21 22:08:01 +0530

    implement co-supervisor request modal part

---
+++ models/cosupervisorRequests.model.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const coSupervisorRequestSchema = new Schema({
+    groupId : {
+        type : String,
+        required : true,
+    },
+
+    email : {
+        type : String,
+        required : true,
+    },
+
+    researchTopic : {
+        type : String,
+        required : true,
+    },
+
+    researchField : {
+        type : String,
+        required : true,
+    },
+
+    coSupervisor : {
+        type : String,
+        required : true,
+    }, 
+
+})
+
+const CoSupervisorRequest = mongoose.model('Request' , coSupervisorRequestSchema);
+module.exports = CoSupervisorRequest;
