08b82800a6d9f6035c302bd914c4a75fa87a3ebf
HKariyawasam <hasanigayani@gmail.com>
2022-05-21 22:44:15 +0530

    created supervisorRequest modal

--- controllers/supervisorRequests.controller.js
+++ controllers/supervisorRequests.controller.js
@@ -1,2 +1,34 @@
 const SupervisorRequestList = require('../models/supervisorRequests.model');
 
+const createSupervisorRequest = async (req, res) => {
+
+    const groupId = req.body.groupId;
+    const email = req.body.email;
+    const researchTopic = req.body.researchField;
+    const supervisor = req.body.supervisor;
+
+
+    const supervisorRequest = new SupervisorRequestList({
+        groupId,
+        email,
+        researchTopic,
+        supervisor
+    })
+
+    try {
+        let response = await supervisorRequest.save();
+        if (response) {
+            return res.status(201).send({ message: 'New Request Sent to the Supervisor' });
+        } else {
+            return res.status(500).send({ message: 'Internal server error' });
+        }
+    } catch (err) {
+        return res.status(400).send({ message: 'Error while sending request to the Supervisor' })
+    }
+
+}
+
+
+module.exports = {
+    createSupervisorRequest
+}
