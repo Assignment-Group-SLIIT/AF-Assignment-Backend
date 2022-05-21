3d6f09ffb7971ce3a12a68d832c335867c4570eb
Thisara Thathsarani <thisararpg@gmail.com>
2022-05-21 22:20:48 +0530

    implement co-supervisor topic  request controller  part

---
+++ controllers/cosupervisorRequests.controller.js
@@ -0,0 +1,34 @@
+const CoSupervisorRequest = require('../models/cosupervisorRequests.model');
+
+const createCoSupervisorRequest = async (req, res) => {
+    const groupId = req.body.groupId;
+    const email = req.body.email;
+    const researchTopic = req.body.researchTopic;
+    const researchField = req.body.researchField;
+	const coSupervisor = req.body.coSupervisor;
+    
+    const newCoSupervisorRequest = new CoSupervisorRequest({
+        groupId,
+        email,
+        coSupervisor,
+        researchTopic,
+        researchField,
+        panelNo
+    })
+
+    try {
+        let response =  await newCoSupervisorRequest.save();
+        if(response) {
+            return res.status(201).send({message: "Register topic to co-supervisor"})
+        }else {
+            return res.status(500).send({message: "Internal server error"})
+        }
+    }catch(err) {
+        return res.status(400).send({message: 'error while register topic'})
+    }
+
+}
+
+module.exports = {
+    createCoSupervisorRequest
+}
