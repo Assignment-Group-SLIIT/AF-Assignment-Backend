d8c4d5b1e801351ac0f420d032ff65a1424c6347
HKariyawasam <hasanigayani@gmail.com>
2022-05-27 16:39:28 +0530

    updated the user controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -158,6 +158,7 @@ const updateUser = async (req, res) => {
 
     const password = user.password;
 
+
     const newUser = {
         fullname: req.body.fullname,
         studentId: req.body.studentId,
@@ -203,6 +204,23 @@ const deleteUser = async (req, res) => {
 
 }
 
+const searchName = async (req, res) => {
+    const name = req.params.name;
+
+    try {
+        const user = await User.findOne({ fullname: name });
+        if (user) {
+            return res.json(user)
+        } else {
+            return res.status(404).send({ message: 'Such user does not exists recheck the email' });
+        }
+
+    } catch (err) {
+        return res.status(500).send({ message: 'Internal Server Error' })
+    }
+
+}
+
 module.exports = {
     register,
     login,
@@ -211,4 +229,5 @@ module.exports = {
     updateUserPassword,
     updateUser,
     deleteUser,
+    searchName
 }
--- routes/user.route.js
+++ routes/user.route.js
@@ -7,6 +7,7 @@ let userController = require('../controllers/user.controller')
 router.post('/register', userController.register);
 router.post('/login', userController.login);
 router.get('/', userController.getAllUsers);
+router.get('/name/:name', userController.searchName);
 router.get('/:email', userController.getOneUser);
 router.put('/:email', userController.updateUser);
 router.put('/:email/:pwd', userController.updateUserPassword);
